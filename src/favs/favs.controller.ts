eaa4855882e381720c8ba9010ac43ddad2307229
Raman Hupalenka <gupalenko.98@mail.ru>
2023-07-26 15:08:31 +0300

    feat: implement basic project structure for api endpoints

---
+++ src/favs/favs.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { FavsService } from './favs.service';
+import { CreateFavDto } from './dto/create-fav.dto';
+import { UpdateFavDto } from './dto/update-fav.dto';
+
+@ApiTags('Favorites')
+@Controller('favs')
+export class FavsController {
+  constructor(private readonly favsService: FavsService) {}
+
+  @Post()
+  create(@Body() createFavDto: CreateFavDto) {
+    return this.favsService.create(createFavDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.favsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.favsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateFavDto: UpdateFavDto) {
+    return this.favsService.update(+id, updateFavDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.favsService.remove(+id);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  try {
+    const swaggerFilePath = resolve(__dirname, '../doc/api.yaml');
+    const swaggerFileData = await readFile(swaggerFilePath);
+    const stringifiedSwaggerData = String(swaggerFileData);
+
+    const document: OpenAPIObject = parse(stringifiedSwaggerData);
+
+    SwaggerModule.setup('/doc', app, document);
+  } catch (err) {
+    const errorMessage = 'Swagger Open API UI setup error: %O';
+
+    console.error(errorMessage, err);
+  }
 
   await app.listen(4000);
 }
