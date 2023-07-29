875ac9d44befdf57331735f9469d6ec234753c3e
Raman Hupalenka <gupalenko.98@mail.ru>
2023-07-29 13:52:15 +0300

    feat: implement favs endpoints logic

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -2,43 +2,86 @@ import {
   Controller,
   Get,
   Post,
-  Body,
-  Patch,
   Param,
   Delete,
+  ClassSerializerInterceptor,
+  Header,
+  HttpCode,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { validateIdParam } from 'src/validators/uuid';
 import { FavsService } from './favs.service';
-import { CreateFavDto } from './dto/create-fav.dto';
-import { UpdateFavDto } from './dto/update-fav.dto';
 
 @ApiTags('Favorites')
 @Controller('favs')
 export class FavsController {
   constructor(private readonly favsService: FavsService) {}
 
-  @Post()
-  create(@Body() createFavDto: CreateFavDto) {
-    return this.favsService.create(createFavDto);
+  @Post('/artist/:id')
+  @Header('Accept', 'application/json')
+  @Header('Content-Type', 'application/json')
+  @UseInterceptors(ClassSerializerInterceptor)
+  addArtist(@Param('id') id: string) {
+    validateIdParam(id);
+
+    return this.favsService.addArtist(id);
+  }
+
+  @Post('/album/:id')
+  @Header('Accept', 'application/json')
+  @Header('Content-Type', 'application/json')
+  @UseInterceptors(ClassSerializerInterceptor)
+  addAlbum(@Param('id') id: string) {
+    validateIdParam(id);
+
+    return this.favsService.addAlbum(id);
+  }
+
+  @Post('/track/:id')
+  @Header('Accept', 'application/json')
+  @Header('Content-Type', 'application/json')
+  @UseInterceptors(ClassSerializerInterceptor)
+  addTrack(@Param('id') id: string) {
+    validateIdParam(id);
+
+    return this.favsService.addTrack(id);
   }
 
   @Get()
+  @Header('Accept', 'application/json')
+  @Header('Content-Type', 'application/json')
+  @UseInterceptors(ClassSerializerInterceptor)
   findAll() {
     return this.favsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.favsService.findOne(+id);
+  @Delete('/artist/:id')
+  @HttpCode(204)
+  @Header('Accept', 'application/json')
+  @Header('Content-Type', 'application/json')
+  removeArtist(@Param('id') id: string) {
+    validateIdParam(id);
+
+    return this.favsService.removeArtist(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFavDto: UpdateFavDto) {
-    return this.favsService.update(+id, updateFavDto);
+  @Delete('/album/:id')
+  @HttpCode(204)
+  @Header('Accept', 'application/json')
+  @Header('Content-Type', 'application/json')
+  removeAlbum(@Param('id') id: string) {
+    validateIdParam(id);
+    return this.favsService.removeAlbum(id);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.favsService.remove(+id);
+  @Delete('/track/:id')
+  @HttpCode(204)
+  @Header('Accept', 'application/json')
+  @Header('Content-Type', 'application/json')
+  removeTrack(@Param('id') id: string) {
+    validateIdParam(id);
+
+    return this.favsService.removeTrack(id);
   }
 }
--- src/favs/favs.service.ts
+++ src/favs/favs.service.ts
@@ -1,26 +1,137 @@
 import { Injectable } from '@nestjs/common';
-import { CreateFavDto } from './dto/create-fav.dto';
-import { UpdateFavDto } from './dto/update-fav.dto';
+import {
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common/exceptions';
+import { AlbumService } from 'src/album/album.service';
+import { ArtistService } from 'src/artist/artist.service';
+import { DBProvider } from 'src/db';
+import { TrackService } from 'src/track/track.service';
+
+// npm run test -- test/favorites.e2e.spec.ts
 
 @Injectable()
 export class FavsService {
-  create(createFavDto: CreateFavDto) {
-    return 'This action adds a new fav';
+  constructor(
+    private readonly artistService: ArtistService,
+    private readonly albumService: AlbumService,
+    private readonly trackService: TrackService,
+    private readonly db: DBProvider,
+  ) {}
+
+  private getArtistById(id: string) {
+    try {
+      return this.artistService.findOne(id);
+    } catch {
+      return null;
+    }
+  }
+
+  private getAlbumById(id: string) {
+    try {
+      return this.albumService.findOne(id);
+    } catch {
+      return null;
+    }
+  }
+
+  private getTrackById(id: string) {
+    try {
+      return this.trackService.findOne(id);
+    } catch {
+      return null;
+    }
+  }
+
+  addArtist(id: string) {
+    const artist = this.getArtistById(id);
+
+    if (artist) {
+      this.db.favorites.artists = this.db.favorites.artists.concat(id);
+
+      return;
+    }
+
+    throw new UnprocessableEntityException();
+  }
+
+  addAlbum(id: string) {
+    const album = this.getAlbumById(id);
+
+    if (album) {
+      this.db.favorites.albums = this.db.favorites.albums.concat(id);
+
+      return;
+    }
+
+    throw new UnprocessableEntityException();
+  }
+
+  addTrack(id: string) {
+    const track = this.getTrackById(id);
+
+    if (track) {
+      this.db.favorites.tracks = this.db.favorites.tracks.concat(id);
+
+      return;
+    }
+
+    throw new UnprocessableEntityException();
   }
 
   findAll() {
-    return `This action returns all favs`;
+    return {
+      artists: this.artistService.findAll().filter((artist) => {
+        return this.db.favorites.artists.includes(artist.id);
+      }),
+      albums: this.albumService.findAll().filter((album) => {
+        return this.db.favorites.albums.includes(album.id);
+      }),
+      tracks: this.trackService.findAll().filter((track) => {
+        return this.db.favorites.tracks.includes(track.id);
+      }),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} fav`;
+  removeArtist(id: string) {
+    const trackId = this.db.favorites.artists.find((trackId) => trackId === id);
+
+    if (trackId) {
+      this.db.favorites.artists = this.db.favorites.artists.filter(
+        (artistId) => artistId !== id,
+      );
+
+      return;
+    }
+
+    throw new NotFoundException();
   }
 
-  update(id: number, updateFavDto: UpdateFavDto) {
-    return `This action updates a #${id} fav`;
+  removeAlbum(id: string) {
+    const albumId = this.db.favorites.albums.find((albumId) => albumId === id);
+
+    if (albumId) {
+      this.db.favorites.albums = this.db.favorites.albums.filter(
+        (albumId) => albumId !== id,
+      );
+
+      return;
+    }
+
+    throw new NotFoundException();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} fav`;
+  removeTrack(id: string) {
+    const trackId = this.db.favorites.tracks.find((trackId) => trackId === id);
+
+    if (trackId) {
+      this.db.favorites.tracks = this.db.favorites.tracks.filter(
+        (trackId) => trackId !== id,
+      );
+
+      return;
+    }
+
+    throw new NotFoundException();
   }
 }
